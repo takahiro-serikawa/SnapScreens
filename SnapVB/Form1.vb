22c85cb8809e35a4737a759866ef512d8a298526
takahiro.serikawa <takahiro.serikawa@gmail.com>
2023-12-13 18:21:20 +0900

    Add project files.

---
+++ SnapVB/Form1.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class Form1
+    Public Sub New()
+        InitializeComponent()
+
+        AddHandler Application.ThreadException, Function(sender, e) _
+            MessageBox.Show(e.Exception.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        AddHandler Me.FormClosed, Function(sender, e) UnregisterHotKey(Me.Handle, hotkey_id)
+
+        ' ホットキーを変えるときはここ
+        RegisterHotKey(Me.Handle, hotkey_id, MODKEY.CONTROL + MODKEY.ALT, Keys.F2)
+    End Sub
+
+    Enum MODKEY
+        ALT = &H1
+        CONTROL = &H2
+        SHIFT = &H4
+    End Enum
+
+    Dim hotkey_id = 1
+
+    Const WM_HOTKEY As Integer = &H312
+
+    Protected Overrides Sub WndProc(ByRef m As Message)
+        MyBase.WndProc(m)
+
+        If m.Msg = WM_HOTKEY AndAlso m.WParam.ToInt32() = hotkey_id Then
+            Dim bounds = Screen.GetBounds(Cursor.Position)
+            Dim captured As New Bitmap(bounds.Width, bounds.Height)
+            Using g = Graphics.FromImage(captured)
+                g.CopyFromScreen(Point.Empty, Point.Empty, bounds.Size)
+            End Using
+            Dim f As New Form2(captured)
+        End If
+    End Sub
+
+    <DllImport("user32.dll")>
+    Shared Function RegisterHotKey(hWnd As IntPtr, id As Integer, fsModifiers As MODKEY, vk As Keys) As Boolean
+    End Function
+
+    <DllImport("user32.dll")>
+    Shared Function UnregisterHotKey(hWnd As IntPtr, id As Integer) As Boolean
+    End Function
+End Class
---
+++ SnapVB/Form2.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form2
+    Public Sub New(bitmap As Bitmap)
+        Me.Text = ""
+        Me.ControlBox = False
+        Me.WindowState = FormWindowState.Maximized
+        Me.TopMost = True
+        Me.Show()
+        AddHandler Me.KeyPress, Sub(sender, e) Me.Close()
+
+        Dim isSelecting = False
+        Dim p1 As Point
+        Dim selRect As Rectangle
+        Dim picturebox1 As New PictureBox With {
+            .Parent = Me,
+            .Size = bitmap.Size,
+            .Image = bitmap,
+            .Cursor = Cursors.Cross
+        }
+
+        AddHandler picturebox1.MouseDown,
+            Sub(sender, e)
+                If e.Button = MouseButtons.Left Then
+                    isSelecting = True
+                    p1 = e.Location
+                End If
+            End Sub
+
+        AddHandler picturebox1.MouseMove,
+            Sub(sender, e)
+                If isSelecting Then
+                    Dim p2 = e.Location
+                    selRect = New Rectangle(Math.Min(p1.X, p2.X), Math.Min(p1.Y, p2.Y),
+                                            Math.Abs(p1.X - p2.X), Math.Abs(p1.Y - p2.Y))
+                    picturebox1.Invalidate()
+                End If
+            End Sub
+
+        AddHandler picturebox1.MouseUp,
+            Sub(sender, e)
+                If e.Button = MouseButtons.Left Then
+                    isSelecting = False
+
+                    Dim cropped As New Bitmap(selRect.Width, selRect.Height)
+                    Using g = Graphics.FromImage(cropped)
+                        g.DrawImage(picturebox1.Image, New Rectangle(0, 0, cropped.Width, cropped.Height), selRect, GraphicsUnit.Pixel)
+                    End Using
+                    Clipboard.SetImage(cropped)
+                End If
+            End Sub
+
+        AddHandler picturebox1.Paint,
+            Sub(sender, e)
+                'Dim ia As New ImageAttributes()
+                'ia.SetColorMatrix(New ColorMatrix With {
+                '                    .Matrix00 = 0.25F, .Matrix11 = 0.25F, .Matrix22 = 0.25F,
+                '                    .Matrix33 = 1, .Matrix44 = 1})
+
+                'e.Graphics.DrawImage(Image, New Rectangle(0, 0, Image.Width, Image.Height),
+                '                    0, 0, Image.Width, Image.Height, GraphicsUnit.Pixel, ia)
+
+                'e.Graphics.DrawImage(Image, selRect.X, selRect.Y, selRect, GraphicsUnit.Pixel)
+
+                Using pen1 As New Pen(Color.Red, 1)
+                    e.Graphics.DrawRectangle(pen1, selRect)
+                End Using
+            End Sub
+
+    End Sub
+End Class
